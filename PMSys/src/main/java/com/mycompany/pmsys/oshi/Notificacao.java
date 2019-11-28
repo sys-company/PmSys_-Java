d1b99640365c45b1a4ad4d0906c88b6ec354a95a
Bloodreina <alexbuarque20@gmail.com>
2019-11-28 20:27:04 -0300

    Atualização textos notificação

--- PMSys/src/main/java/com/mycompany/pmsys/oshi/Notificacao.java
+++ PMSys/src/main/java/com/mycompany/pmsys/oshi/Notificacao.java
@@ -19,82 +19,80 @@ public class Notificacao {
     public static void notificacaoCPU(Double totalUsado, int idMaquina){
         
         if(totalUsado > 75.00){
-            String mensagem = "A taxa de uso da CPU está acima de 75%!";
+            String mensagem = "A taxa de uso da CPU(" + totalUsado + ") está acima de 75%!";
             String queryIdFuncionario = "(select idFuncionario from tblFuncionario where fkMaquina = " + idMaquina + ")";
             String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
             try{
                 
                 dataCapturada = new Date();
-                //jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
-                System.out.println("Foi CPU");
-                //GerarLog.escreverLog("Notificação de CPU gerada!", "B");
+                jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
+                
+                GerarLog.escreverLog(mensagem, "B");
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B");
             }
         }
-        System.out.println("Foi CPU");
         
     }
     
     public static void notificacaoRAM(Double totalRAM, double totalUsado, int idMaquina){
-        String mensagem = "A taxa de uso de RAM está acima do esperado!";
+
         String queryIdFuncionario = "(select idFuncionario from tblFuncionario where fkMaquina = " + idMaquina + ")";
         String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
         
         double primeiroQ = (totalRAM * 25) / 100;
         double terceiroQ = (totalRAM * 75) / 100;        
+
+        String mensagem = "A taxa de uso de RAM( " + totalUsado + " ) está acima do esperado( " + terceiroQ + " )!";
         
         if(totalUsado > terceiroQ){
             try{
                 dataCapturada = new Date();
-                //jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
-                System.out.println("Foi RAM");
-                //GerarLog.escreverLog("Notificação de RAM gerada!", "B");
+                jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
+
+                GerarLog.escreverLog(mensagem, "B");
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B");
             }
         }
-        System.out.println("Foi RAM");
     }
     
     public static void notificacaoHD(Double espacoTotal, double totalDisponivel, int idMaquina){
-        String mensagem = "Espaço em HD disponivel é menor que o mínimo indicado!";
+        
         String queryIdFuncionario = "(select idFuncionario from tblFuncionario where fkMaquina = " + idMaquina + ")";
         String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
         
         double primeiroQ = (espacoTotal * 25) / 100;
         double terceiroQ = (espacoTotal * 75) / 100;        
         
+        String mensagem = "Espaço em HD disponivel( " + totalDisponivel + ") é menor que o mínimo( " + (espacoTotal - terceiroQ) + ") indicado!";
         if(totalDisponivel > terceiroQ){
             try{
                 dataCapturada = new Date();
-                //jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
-                System.out.println("Foi HD");
-                //GerarLog.escreverLog("Notificação de HD gerada!", "B");
+                jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
+
+                GerarLog.escreverLog(mensagem, "B");
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B");
             }
         }
-        System.out.println("Foi Processo");
     }
     
     public static void notificacaoProcesso(Long horas, String processo,int idMaquina){
-        String mensagem = "O processo " + processo + " está a mais de 8 horas em uso!";
+        String mensagem = "O processo " + processo + " está hà" + horas + "horas em uso!";
         String queryIdFuncionario = "(select idFuncionario from tblFuncionario where fkMaquina = " + idMaquina + ")";
         String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
         
         if(horas > 8){
             try{
                 dataCapturada = new Date();
-                //jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
-                System.out.println("Foi Processo");
-                //GerarLog.escreverLog("Notificação de CPU gerada!", "B");
+                jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
+
+                GerarLog.escreverLog(mensagem, "B");
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B");
             }
         }
-        
-        System.out.println("Foi Processo");
     }
     
     
