606deaeb02e0c8c3728df5ad2296191fc95eb02c
Aluno <henriquee.matos@hotmail.com>
2019-12-02 19:19:27 -0200

    enviando senha via email

--- PMSys/src/main/java/com/mycompany/pmsys/Email.java
+++ PMSys/src/main/java/com/mycompany/pmsys/Email.java
@@ -17,51 +17,48 @@ import javax.mail.internet.*;
 public class Email {
 
     public void sendEmail(String email, String senha) {
-        
+
         String to = email;//change accordingly  
         String from = "dotsys.company@gmail.com";//change accordingly  
-         
 
         //Get the session object  
         Properties properties = new Properties();
         properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.port", "587");
         properties.put("mail.smtp.socketFactory.class",
-                "javax.netssl.SSLSocketFactory");
-        properties.put("mail.smtp.auth", "ture");
-        properties.put("mail.smtp.port", "465");
+                "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        
 
         Session session = Session.getDefaultInstance(properties,
                 new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication()
-
-            {
+            protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication("dotsys.company@gmail.com",
                         "#Gfgrupo6");
             }
         });
 
-    session.setDebug(true);
+        session.setDebug(true);
 
-    
+        //compose the message  
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Enviando email de recuperação de senha");
+            message.setText("Olá " + email + ", este é um email de recuperação de senha"
+                    + "\n sua senha é: " + senha);
 
-    //compose the message  
-    try{  
-         Message message = new MimeMessage(session);  
-         message.setFrom(new InternetAddress(from));  
-         message.setRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-         message.setSubject("Enviando email de recuperação de senha");  
-         message.setText("Olá "+email+", este é um email de recuperação de senha"
-                 + "\n sua senha é: "+senha);  
-  
-         // Send message  
-         Transport.send(message);  
-         System.out.println("Mensagem enviada com sucesso....");  
-  
-      }catch (MessagingException mex
+            // Send message  
+            Transport.send(message);
+            System.out.println("Mensagem enviada com sucesso....");
 
-    
-        ) {mex.printStackTrace();
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
-}
 }
