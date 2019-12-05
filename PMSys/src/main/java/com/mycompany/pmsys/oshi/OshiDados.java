c0deb55ccee1652161e5c6c7cd78cca009885027
Bloodreina <alexbuarque20@gmail.com>
2019-12-05 17:49:21 -0300

    Pequenas mudanças pos apresentação

--- PMSys/src/main/java/com/mycompany/pmsys/oshi/OshiDados.java
+++ PMSys/src/main/java/com/mycompany/pmsys/oshi/OshiDados.java
@@ -11,6 +11,9 @@ package com.mycompany.pmsys.oshi;
  */
 
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import log.GerarLog;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -21,24 +24,27 @@ public class OshiDados implements Job{
 
 
     public void execute(JobExecutionContext args0) throws JobExecutionException {
-        Integer idMaquina = 1009;
+        Integer idMaquina = 1015;
         DadosCPU dCpu = new DadosCPU(idMaquina);
         DadosRAM dRam = new DadosRAM(idMaquina);
         DadosHD dHd = new DadosHD(idMaquina);
         DadosProcessos dProcessos = new DadosProcessos(idMaquina);
+        Format formatador = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        Date dt = new Date();
+        String dataLog = formatador.format(dt);
         
         try{
         GerarLog.escreverLog("Inserindo dados de CPU...", "B", idMaquina);
-            System.out.println("Inserindo dados de CPU...");
+            System.out.println(dataLog + " | Inserindo dados de CPU...");
         dCpu.insereDadosCPU();
         GerarLog.escreverLog("Inserindo dados de RAM...", "B", idMaquina);
-            System.out.println("Inserindo dados de RAM...");
+            System.out.println(dataLog + " | Inserindo dados de RAM...");
         dRam.insereDadosRam();
         GerarLog.escreverLog("Inserindo dadosde HD...", "B", idMaquina);
-            System.out.println("Inserindo dadosde HD...");
+            System.out.println(dataLog + " | Inserindo dadosde HD...");
         dHd.insereDadosHD();
         GerarLog.escreverLog("Enviando processos...", "B", idMaquina);
-            System.out.println("Enviando processos...");
+            System.out.println(dataLog + " | Enviando processos...");
         dProcessos.processosAtuais();
         }catch(Exception e){
             GerarLog.escreverLog("Erro ao inserir dados : " +e, "B", idMaquina);
