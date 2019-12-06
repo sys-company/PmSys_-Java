d31e4d9549e198ebe4e1d5258c94eaefa0c32265
Bloodreina <alexbuarque20@gmail.com>
2019-12-06 18:00:16 -0300

    Atualização notificãções

--- PMSys/src/main/java/com/mycompany/pmsys/Slack.java
+++ PMSys/src/main/java/com/mycompany/pmsys/Slack.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.pmsys;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -17,12 +12,12 @@ import log.GerarLog;
 
 /**
  *
- * @author marce
+ * @author marcelo
  */
 public class Slack {
     
     public Boolean enviarMensagem(String mensagem, String identificador, String colaborador) {
-        System.out.println(mensagem);
+        
         if (mensagem.isBlank() || identificador.isBlank()) return false;
         try {
             URL url = new URL("https://hooks.slack.com/services/TMNE3H26A/"
@@ -47,9 +42,6 @@ public class Slack {
                 GerarLog.escreverLog("Foi enviada a mensagem: '" + mensagem + "' para o colaborador: " + colaborador, "A", 0);
                 return true;
             }
-            
-            
-            
         }
         catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Erro ao enviar mensagem para o slack: " + e.getMessage());
--- PMSys/src/main/java/com/mycompany/pmsys/oshi/Notificacao.java
+++ PMSys/src/main/java/com/mycompany/pmsys/oshi/Notificacao.java
@@ -2,7 +2,10 @@
 package com.mycompany.pmsys.oshi;
 
 import com.mycompany.pmsys.ConnectURL;
+import com.mycompany.pmsys.Slack;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import log.GerarLog;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -15,17 +18,35 @@ public class Notificacao {
     private static ConnectURL dadosConexao = new ConnectURL();
     private static JdbcTemplate jdbcTemplate = new JdbcTemplate(dadosConexao.getDataSource());
     private static Date dataCapturada;
+    private static String identificador = "";
+    private static String nome = "";
+    
+    private static void buscaIdentificador(String mensagem, Integer maquina){
+        String stringSQL = "SELECT nomeFuncionario, identificador from tblFuncionario where fkMaquina = ?";
+        
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(stringSQL, maquina);
+        
+        for(Map row : rows){
+            identificador = row.get("identificador").toString();
+            nome = row.get("nomeFuncionario").toString();
+        }
+        
+        notificarSlack(mensagem, identificador, nome);
+       
+    }
     
     public static void notificacaoCPU(Double totalUsado, int idMaquina){
         
         if(totalUsado > 75.00){
-            String mensagem = "A taxa de uso da CPU(" + totalUsado + ") está acima de 75%!";
+            String mensagem = "A taxa de uso da CPU(" + formataValor(totalUsado) + ") está acima de 75%!";
             String queryIdFuncionario = "(select idFuncionario from tblFuncionario where fkMaquina = " + idMaquina + ")";
             String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
             try{
                 
                 dataCapturada = new Date();
                 jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
+                
+                buscaIdentificador(mensagem, idMaquina);
                 
                 GerarLog.escreverLog(mensagem, "B", idMaquina);
             }catch(Exception ex){
@@ -41,15 +62,18 @@ public class Notificacao {
         String queryIdSquad = "(select fkSquad from tblFuncionario where fkMaquina = " + idMaquina + ")";
         
         double primeiroQ = (totalRAM * 25) / 100;
-        double terceiroQ = (totalRAM * 75) / 100;        
-
-        String mensagem = "A taxa de uso de RAM( " + totalUsado + " ) está acima do esperado( " + terceiroQ + " )!";
+        double terceiroQ = (totalRAM * 75) / 100;  
         
+        String mensagem = "A taxa de uso de RAM( " + formataValor(totalUsado) + " ) está acima do esperado( " + formataValor(terceiroQ) + " )!";
+        //buscaIdentificador(mensagem, idMaquina);
+        System.out.println(mensagem);
         if(totalUsado > terceiroQ){
             try{
                 dataCapturada = new Date();
                 jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
 
+                
+                
                 GerarLog.escreverLog(mensagem, "B", idMaquina);
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B", idMaquina);
@@ -65,12 +89,15 @@ public class Notificacao {
         double primeiroQ = (espacoTotal * 25) / 100;
         double terceiroQ = (espacoTotal * 75) / 100;        
         
-        String mensagem = "Espaço em HD disponivel( " + totalDisponivel + ") é menor que o mínimo( " + (espacoTotal - terceiroQ) + ") indicado!";
+        String mensagem = "Espaço em HD disponivel( " + formataValor(totalDisponivel) + ") é menor que o mínimo( " + formataValor(espacoTotal - terceiroQ) + ") indicado!";
+        
         if(totalDisponivel > terceiroQ){
             try{
                 dataCapturada = new Date();
                 jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
 
+                buscaIdentificador(mensagem, idMaquina);
+                
                 GerarLog.escreverLog(mensagem, "B", idMaquina);
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B", idMaquina);
@@ -87,7 +114,9 @@ public class Notificacao {
             try{
                 dataCapturada = new Date();
                 jdbcTemplate.update("INSERT INTO tblNotificacao values (?, ?, "+ queryIdFuncionario + ", ?, " + queryIdSquad + ")", mensagem, dataCapturada, 3);
-
+                   
+                buscaIdentificador(mensagem, idMaquina);
+                
                 GerarLog.escreverLog(mensagem, "B", idMaquina);
             }catch(Exception ex){
                 GerarLog.escreverLog("Erro ao gerar notificação da CPU", "B", idMaquina);
@@ -95,13 +124,27 @@ public class Notificacao {
         }
     }
     
+    private static String formataValor(Double v1){
+        String aux = String.valueOf(v1);
+        String valor1 = aux.substring(0,1);
+        String valor2 = aux.substring(1,2);
+        return valor1 + "." + valor2;
+    }
     
+    private static void notificarSlack(String mensagem, String identificador, String nome){
+        
+        Slack s = new Slack();
+        s.enviarMensagem(mensagem, identificador, nome);
+        
+    }
     
     public static void main(String[] args) {
     //    notificacaoCPU(76.00, 1000);
-    //    notificacaoRAM(900.00, 668.00, 1001);
-    //    notificacaoHD(916.00, 600, 1006);
+    //    notificacaoRAM(8000.0, 6680.0, 1000);
+    //   notificacaoHD(916.00, 600, 1006);
     //    notificacaoProcesso(Long.parseLong("5"), "The Forest", 1004);
+    
+    //   System.out.println(buscaIdentificador(1000));
     }
     
 }
